9aab044918b4dc243a47aaa4f603bdb85dfe9022
Jack Wakefield <jack.wakefield@alsoasked.com>
2023-03-13 19:34:18 +0000

    Add a description to the API key security scheme

--- lint-prose.sh
+++ lint-prose.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-input=$(npm exec -- widdershins --user_templates widdershins/openapi3/ --omitBody --omitHeader --summary openapi/openapi.yaml | sed -n '/Compiling main.dot to function/,$p' | sed -n '1,/Compiling main.dot to function/d; p' | sed '/^$/d')
+input=$(npm exec -- widdershins --user_templates widdershins/openapi3/ --omitBody --omitHeader --summary openapi/openapi.yaml | sed -n '/Compiling main.dot to function/,$p' | sed -n '1,/Compiling main.dot to function/d; p')
 
 echo "Input:"
 echo ""
-echo "$input" | nl
+echo "$input" | nl -ba
 
 echo ""
 echo "Results:"
