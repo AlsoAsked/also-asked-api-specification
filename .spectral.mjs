0d3eec440a566a4a0b3a25041a5ce270ad025abf
Jack Wakefield <jackwakefield@protonmail.com>
2023-08-21 13:44:00 +0100

    Use relative URLs for the images

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4OTM4MQ/spectral.js?branch_slug=documentation&token=c80e2304-008a-4507-ab6b-1de2cedb53f0";
+export default { extends: ruleset };
